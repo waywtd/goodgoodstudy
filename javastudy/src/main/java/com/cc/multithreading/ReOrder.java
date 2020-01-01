d190541f07f49fd54b62a495fd19fb5cf53bbe2b
chenc <chenc@jiguang.cn>
2020-01-01 22:56:03 +0800

    重排序

--- javastudy/src/main/java/com/cc/multithreading/ReOrder.java
+++ javastudy/src/main/java/com/cc/multithreading/ReOrder.java
@@ -1,4 +1,68 @@
 package com.cc.multithreading;
 
 public class ReOrder {
+    int a = 0;
+    boolean flag = false;
+
+    public void write() {
+        a = 1;
+        flag = true;
+    }
+
+    public void review() {
+        a = 0;
+        flag = false;
+    }
+
+    public void read() {
+        if (flag) {
+            int result = a * a;
+            System.out.println(result);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReOrder reOrder = new ReOrder();
+        class Writer implements Runnable {
+
+            private final ReOrder reOrder;
+
+            public Writer(ReOrder reOrder) {
+                this.reOrder = reOrder;
+            }
+
+            public void run() {
+                for(int i = 0; i < 10000; i++) {
+                    reOrder.write();
+                    reOrder.review();ke
+                }
+            }
+        }
+
+        class Reader implements Runnable {
+
+            private final ReOrder reOrder;
+
+            public Reader(ReOrder reOrder) {
+                this.reOrder = reOrder;
+            }
+
+            public void run() {
+                for(int i = 0; i < 10000; i++) {
+                    reOrder.read();
+                }
+            }
+        }
+
+        Writer writer = new Writer(reOrder);
+        Reader reader = new Reader(reOrder);
+        Thread writerThread = new Thread(writer);
+        writerThread.start();
+        Thread readerThread = new Thread(reader);
+        readerThread.start();
+
+        writerThread.join();
+        readerThread.join();
+
+    }
 }
